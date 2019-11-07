ed13b2cc8b72de66f5b4a456664185a20c7fc89b
Eduard Urbach <e.urbach@gmail.com>
2019-11-07 13:38:07 +0900

    Improved syscalls

--- syscall.go
+++ syscall.go
@@ -12,7 +12,7 @@ func (a *Assembler) Print(msg string) {
 	a.MoveRegisterNumber(syscall.Registers[1], 1)
 	a.MoveRegisterAddress(syscall.Registers[2], address)
 	a.MoveRegisterNumber(syscall.Registers[3], uint64(len(msg)))
-	a.WriteBytes(0x0f, 0x05)
+	a.Syscall()
 }
 
 // Print prints a message followed by a new line on the terminal.
@@ -24,7 +24,7 @@ func (a *Assembler) Println(msg string) {
 func (a *Assembler) Exit(code int32) {
 	a.MoveRegisterNumber(syscall.Registers[0], uint64(syscall.Exit))
 	a.MoveRegisterNumber(syscall.Registers[1], uint64(code))
-	a.WriteBytes(0x0f, 0x05)
+	a.Syscall()
 }
 
 // Open opens a file.
@@ -36,5 +36,10 @@ func (a *Assembler) Open(fileName string) {
 	a.MoveRegisterAddress(syscall.Registers[2], address)
 	a.MoveRegisterNumber(syscall.Registers[3], uint64(0102))
 	a.MoveRegisterNumber(syscall.Registers[4], uint64(0666))
+	a.Syscall()
+}
+
+// Syscall executes a syscall.
+func (a *Assembler) Syscall() {
 	a.WriteBytes(0x0f, 0x05)
 }
