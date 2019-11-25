09e09c786d9d8fe91ab57cad2f79099bcfe2f512
Eduard Urbach <e.urbach@gmail.com>
2019-11-25 12:45:08 +0900

    Implemented CompareRegisterRegister

--- compare.go
+++ compare.go
@@ -4,3 +4,8 @@ package asm
 func (a *Assembler) CompareRegisterNumber(registerName string, number uint64) {
 	a.numberToRegisterSimple(0x83, 0x80, 0x3c, 0b111, false, registerName, number)
 }
+
+// CompareRegisterRegister compares a register with a register.
+func (a *Assembler) CompareRegisterRegister(registerNameA string, registerNameB string) {
+	a.registerToRegister([]byte{0x39}, []byte{0x38}, registerNameA, registerNameB, false)
+}
